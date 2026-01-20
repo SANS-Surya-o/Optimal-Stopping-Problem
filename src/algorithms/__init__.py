7763334a722c7ebe3c41d31e7628b68f33086486
sriyansh <sriyanshsuryadevara@gmail.com>
2026-01-20 19:14:45 +0530

    lsm improvements

---
+++ src/algorithms/__init__.py
@@ -0,0 +1,23 @@
+from .dqn import DQNAgent, linear_decay_schedule
+from .q_learning import (QLearningAgent, 
+                                   QLearningAgentConstraint, 
+                                   QLearningAgentVanilla,
+                                   QOptimalBehaviourAgent,
+                                   DoubleQLearningAgent,
+                                   QSuperGreedyAgent,
+                                   exponential_decay_schedule
+                                   )
+from .longstaff_schwartz import LongstaffSchwartzAgentBuySell
+
+__all__ = [
+    "DQNAgent",
+    "linear_decay_schedule",
+    "QLearningAgent",
+    "QLearningAgentConstraint",
+    "QLearningAgentVanilla",
+    "QOptimalBehaviourAgent",
+    "DoubleQLearningAgent",
+    "QSuperGreedyAgent",
+    "exponential_decay_schedule",
+    "LongstaffSchwartzAgentBuySell"
+]
